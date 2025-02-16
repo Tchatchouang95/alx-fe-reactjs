8dcc6b60592c67bacb6af762150ec7c685407a19
Tchatchouang95 <cedrickdjopgang@gmail.com>
2025-02-16 17:38:41 +0100

    Creating a header, main and footer components and inserting to the App component

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,13 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
+import Header from './Header'
+import MainContent from './MainContent'
+import Footer from './Footer'
 
 function App() {
 
   return (
-    <WelcomeMessage />
+    <>
+      <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
+    </>
+    
   )
 }
 
