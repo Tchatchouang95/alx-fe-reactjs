77cf5c62ee782b4876bb759247878e0a9a527abe
Tchatchouang95 <cedrickdjopgang@gmail.com>
2025-02-23 21:06:46 +0100

    Styling my react app

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer style={{textAlign: 'center', backgroundColor: 'blueviolet', margin: '20px 0'}}>
+            <p style={{color: 'white', padding: '20px'}}>© 2023 City Lovers</p>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+    return (
+        <main style={{border: '1px solid blue'}}>
+            <p style={{textAlign: 'center', fontStyle: 'italic'}}>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    )
+}
+
+export default MainContent;
