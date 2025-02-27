f4b8fc3532d260017a96798ec8e312d539d6e970
Tchatchouang95 <cedrickdjopgang@gmail.com>
2025-02-27 14:58:59 +0100

    my-company app

---
+++ my-company/src/About.jsx
@@ -0,0 +1,12 @@
+import './StyleApp.css'
+
+function About() {
+    return (
+      <div className='size'>
+        <h1 style={{fontSize: '60px', textAlign: 'center'}}>About Us</h1>
+        <p>Our company has been providing top-notch services since 1990. We specialize in various fields including technology, marketing, and consultancy.</p>
+      </div>
+    );
+  }
+
+  export default About;
