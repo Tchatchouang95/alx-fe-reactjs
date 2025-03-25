2851495662ce558538753c196cd59688b1be79cb
Tchatchouang95 <cedrickdjopgang@gmail.com>
2025-03-25 15:00:14 +0100

    Added tailwind config files

--- tailwind-react-integration/vite.config.js
+++ tailwind-react-integration/vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  darkMode: false,
-  theme: { extend: {} },
-  variants: { extend: {} },
   plugins: [react()],
 })
