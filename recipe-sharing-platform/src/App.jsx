8b7768c5b87f94c721d45cf600e5b7e9b85dc879
Tchatchouang95 <cedrickdjopgang@gmail.com>
2025-03-27 19:12:45 +0100

    Building a responsive homepage of the recipe sharing platform

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,9 +1,10 @@
+import HomePage from "./components/HomePage"
 
 function App() {
 
   return (
     <div>
-      <h1 className="bg-gray-500 text-center p-8 text-white text-3xl">Hello World</h1>
+      <HomePage />
     </div>
   )
 }
---
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import data from '../data.json';
+
+function HomePage() {
+
+    const [recipes, setRecipes] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setRecipes(data);
+        }
+        fetchData();
+    }, [])
+
+  return (
+    <div className='mx-auto max-w-250 bg-gray-100'>
+        <h1 className='text-4xl font-bold text-center p-4 shadow-sm text-gray-600'>Recipe Sharing App</h1>
+        <h3 className='font-bold text-gray-500 ml-2 mt-5'>Recipe List</h3>
+        <div className='grid lg:grid-cols-3 sm:grid-cols-2 mt-5 gap-2'>
+            {recipes.map(recipe => (
+                <div key={recipe.id} className='bg-white m-2 rounded overflow-hidden shadow-md hover:shadow-2xl'>
+                    <img src= {recipe.image} className='w-full hover:scale-110 transition-transform duration-300 ease-in-out' alt={recipe.title} />
+                    <h3 className='font-bold text-2xl text-gray-600 m-4'>{recipe.title}</h3>
+                    <p className='text-gray-500 m-4 text-sm'>{recipe.summary}</p>
+                </div>
+            ))}
+        </div>
+    </div>
+  )
+}
+
+export default HomePage;
