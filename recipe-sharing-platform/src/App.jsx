9b48b39d49031732804cd5909b44f8ce2a560a29
Tchatchouang95 <cedrickdjopgang@gmail.com>
2025-03-31 13:37:50 +0100

    Included routing to recipe details

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,11 +1,16 @@
 import HomePage from "./components/HomePage"
+import RecipeDetail from "./components/RecipeDetail"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 
 function App() {
 
   return (
-    <div>
-      <HomePage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/recipe/:id" element={<RecipeDetail />} />
+      </Routes>
+    </Router>
   )
 }
 
